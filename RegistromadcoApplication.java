d544c1778173e55945f3d1995dae726b7685ab68
Sayuli-P <31893288+Sayuli-P@users.noreply.github.com>
2019-12-04 11:13:29 -0600

    Add files via upload

---
+++ RegistromadcoApplication.java
@@ -0,0 +1,13 @@
+package com.core.madco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistromadcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistromadcoApplication.class, args);
+	}
+
+}
